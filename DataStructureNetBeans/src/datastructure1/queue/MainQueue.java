b97c836b911cd6f745c1dc179570cbc3b5eaf426
Jorge Samuel Teixeira Jordão <116199476+JorgeSTJordao@users.noreply.github.com>
2024-08-11 01:16:07 -0300

    :file_folder: Organized all files and folders from both IDEs

--- DataStructureNetBeans/src/datastructure1/queue/MainQueue.java
+++ DataStructureNetBeans/src/datastructure1/queue/MainQueue.java
@@ -11,16 +11,27 @@ package datastructure1.queue;
 public class MainQueue {
         public void execute(){
         Queue queue = new Queue(3);
+        
         queue.dequeue();
+        
         queue.enqueue(1);
         queue.enqueue(2);
         queue.enqueue(3);
+        
         queue.print();
-        queue.enqueue(1);
+        
+        queue.enqueue(4);
+        
         queue.dequeue();
-        queue.dequeue();
-        queue.dequeue();
+        
         queue.print();
+        
+        queue.dequeue();
+                
+        queue.dequeue();
+        
+        queue.print();
+        
         queue.dequeue();
 
     }
